d77b3b468f6f1a58767a3a54182f077d7a4e671f
Dmitriy <dmitry.shebetovsky@gmail.com>
2019-04-29 23:36:08 +0300

    Added screenshots

---
+++ BlinkWebCam/BlinkLed.cs
@@ -0,0 +1,36 @@
+﻿namespace BlinkWebCam
+{
+    class BlinkLed
+    {
+        private const int WM_CAP_DRIVER_CONNECT = 0x40a;
+        private const int WM_CAP_DRIVER_DISCONNECT = 0x40b;
+        private const int WS_CHILD = 0x40000000;
+        private const int WS_POPUP = unchecked((int)0x80000000);
+        private const int WM_CAP_SAVEDIB = 0x419;
+
+        [System.Runtime.InteropServices.DllImport("avicap32.dll", EntryPoint = "capCreateCaptureWindowA")]
+        public static extern System.IntPtr capCreateCaptureWindowA(string lpszWindowName, int dwStyle, int X, int Y, int nWidth, int nHeight, int hwndParent, int nID);
+
+        [System.Runtime.InteropServices.DllImport("user32", EntryPoint = "SendMessage")]
+        public static extern int SendMessage(System.IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+        public void ControllCamWithoutScreenshot()
+        {
+            System.IntPtr hWndC = capCreateCaptureWindowA("VFW Capture", WS_POPUP | WS_CHILD, 0, 0, 320, 240, 0, 0); // get camera handle
+            SendMessage(hWndC, WM_CAP_DRIVER_CONNECT, 0, 0); // connect to camera
+            SendMessage(hWndC, WM_CAP_DRIVER_DISCONNECT, 0, 0); // turn off the camera
+        }
+
+        public void ControllCamWithScreenshot()
+        {
+            string dName = "".PadRight(100);
+            string dVersion = "".PadRight(100);
+            System.IntPtr hWndC = capCreateCaptureWindowA("VFW Capture", WS_POPUP | WS_CHILD, 0, 0, 320, 240, 0, 0); // get camera handle
+            SendMessage(hWndC, WM_CAP_DRIVER_CONNECT, 0, 0); // connect to camera
+            string path = System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\Pictures\\" + System.DateTime.Now.ToString("yyyy.MM.dd HH.mm.ss") + ".jpg";
+            System.IntPtr hBmp = System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi(path);
+            SendMessage(hWndC, WM_CAP_SAVEDIB, 0, hBmp.ToInt32()); // save screenshot
+            SendMessage(hWndC, WM_CAP_DRIVER_DISCONNECT, 0, 0); // turn off the camera
+        }
+    }
+}
--- BlinkWebCam/Program.cs
+++ BlinkWebCam/Program.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlinkWebCam
+﻿namespace BlinkLedWebCam
 {
     class Program
     {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool ShowWindow(System.IntPtr hWnd, int nCmdShow);
+
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern System.IntPtr GetConsoleWindow();
         static void Main(string[] args)
         {
+            ShowWindow(GetConsoleWindow(), 0);
+            System.Windows.Forms.NotifyIcon icon = new System.Windows.Forms.NotifyIcon();
+            icon.Icon = new System.Drawing.Icon("WebcamIcon.ico");
+            icon.Visible = true;
+            icon.Text = "Blink webcam led\n\nClick for open app\nDouble click for close app";
+            icon.ShowBalloonTip(1000, "I'm here now!", "In tray", System.Windows.Forms.ToolTipIcon.Info);
+            icon.DoubleClick += (s, e) => { System.Environment.Exit(0); };
+            icon.MouseClick += (s, e) => { ShowWindow(GetConsoleWindow(), 1); icon.Visible = false;};
+
+            System.Console.Title = "Blink webcam led";
+            System.Console.WindowWidth = 30;
+            System.Console.WindowHeight = 2;
+            System.Console.BackgroundColor = System.ConsoleColor.Blue;
+            System.Console.ForegroundColor = System.ConsoleColor.Green;
+
+            BlinkWebCam.BlinkLed blinkLed = new BlinkWebCam.BlinkLed();
+            int milliseconds = 2000;
+            System.Console.WriteLine("Press ESC for exit...");
+
+            while (!System.Console.KeyAvailable)
+            {
+                blinkLed.ControllCamWithoutScreenshot();
+                System.Threading.Thread.Sleep(milliseconds);
+            }
+            while (System.Console.ReadKey(true).Key != System.ConsoleKey.Escape);
         }
     }
 }
